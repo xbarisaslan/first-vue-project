51e5685f34aae8867d37575aa96876baba54c7d1
Baris <baslan374@gmail.com>
2022-07-03 13:00:05 +0300

    Import task component

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,20 @@
 <template>
-    <div v-for="task in tasks">
-    <h3>{{task.text}}</h3>
+    <div :key="task.id" v-for="task in tasks">
+        <Task @delete-task="$emit('delete-task' , task.id)" :task="task"/>
     </div>
 </template>
 
 <script>
+    import Task from './Task'
+
     export default {
         name: "Tasks",
         props: {
             tasks: Array,
-        }
+        } ,
+        components: {
+            Task,
+        },
+        emits: ['delete-task']
     }
 </script>
