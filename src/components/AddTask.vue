40cbf5f8f72b46c2890f68119aa34f9a5afb0fd1
Baris <baslan374@gmail.com>
2022-07-04 21:16:40 +0300

    Initial commit

---
+++ src/components/AddTask.vue
@@ -0,0 +1,22 @@
+<template>
+    <form class="add-form"></form>
+      <div class="form-control">
+        <label>Task</label>
+        <input type="text" name="text" placeholder="Add Task" />
+      </div>
+      <div class="form-control">
+        <label>Day & Time</label>
+        <input type="text" name="day" placeholder="Add Day & Time" />
+      </div>
+      <div class="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input type="checkbox" name="reminder" />
+      </div>
+
+      <input class="btn btn-block" type="submit" value="Save Task" />
+    </form>
+</template>
+
+<script>
+
+</script>
